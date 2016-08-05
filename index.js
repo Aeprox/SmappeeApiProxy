14ef44b59202b5277475bb75c49567a5b0ae7ceb
aeprox <aeprox@aeprox.net>
2016-08-05 12:21:09 +0200

    Made host and port flexible w

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var request = require('request');
 var express = require('express');
+var mustache = require('mustache');
+var fs = require('fs');
 var app = express();
 
 var ipAddress = '192.168.1.244';
 var passwd = 'admin'
-
+var filePath = __dirname + '/public/smappee.html';
 
 
 app.get('/api', function (req, res) {
@@ -30,9 +32,22 @@ app.get('/api', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-	res.sendFile('smappee.html', {root: __dirname + '/public'});
+	var apiUrl = req.protocol + "://" + req.get('host') + req.originalUrl + "api";
+	console.log(apiUrl);
+	fs.readFile(filePath, function (err, content) {
+		res.send(mustache.render(content.toString(), {'ApiURL' : apiUrl}))
+	});
+	
+});
+
+
+app.use(function(req, res, next) {
+	// CORS
+	res.header("Access-Control-Allow-Origin", "*");
+
+    next();
 });
 
 app.listen(3000, function () {
-  console.log('Smappiapi Proxy listening on port 3000!');
+	console.log('Smappiapi Proxy listening on port 3000!');
 });
